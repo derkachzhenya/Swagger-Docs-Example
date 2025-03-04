aa3e8db162bc823489c48125ffe1651dee730c5f
Yevhen <zheka4573@gmail.com>
2025-03-04 15:21:31 +0200

    swagger with jwt

--- app/Http/Controllers/Swagger/PostController.php
+++ app/Http/Controllers/Swagger/PostController.php
@@ -4,27 +4,215 @@ namespace App\Http\Controllers\Swagger;
 
 use App\Http\Controllers\Controller;
 
-
-
 /**
  * @OA\Post(
  *     path="/api/posts",
- *     summary="Создание",
+ *     summary="Создание поста",
+ *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
  * 
  *     @OA\RequestBody(
- * 
+ *         required=true,
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="title",
+ *                 type="string",
+ *                 example="Some body"
+ *             ),
+ *              @OA\Property(
+ *                 property="like",
+ *                 type="integer",
+ *                 example=20
+ *             ),
+ *         )
  *     ),
  * 
  *     @OA\Response(
  *         response=200,
  *         description="OK",
+ *         @OA\JsonContent(
+ *              @OA\Property(
+ *                 property="data",
+ *                 type="object",  
+ *                  @OA\Property(
+ *                      property="id",
+ *                      type="integer",
+ *                      example=1
+ *                  ),
+ *                  @OA\Property(
+ *                      property="title",
+ *                      type="string",
+ *                      example="Some body"
+ *                  ),
+ *                  @OA\Property(
+ *                      property="like",
+ *                      type="integer",
+ *                      example=20
+ *                   ),
+ *               ),
+ *          )
+ *     )
+ * ),
+ * 
+ * @OA\Get(
+ *     path="/api/posts",
+ *     summary="Список",
+ *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
+ * 
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *              @OA\Property(
+ *                 property="data",
+ *                 type="array",
+ *                 @OA\Items(
+ *                     @OA\Property(
+ *                          property="id",
+ *                          type="integer",
+ *                          example=1
+ *                      ),
+ *                      @OA\Property(
+ *                          property="title",
+ *                          type="string",
+ *                          example="Some body"
+ *                      ),
+ *                      @OA\Property(
+ *                          property="like",
+ *                          type="integer",
+ *                          example=20
+ *                      ),
+ *                  ),
+ *              ),               
+ *          )
+ *     )
+ * ),
+ * 
+ * @OA\Get(
+ *     path="/api/posts/{post}",
+ *     summary="Единичная запись",
+ *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
+ *     @OA\Parameter(
+ *         description="ID поста",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=1,
  *     ),
- * )
  * 
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *              @OA\Property(
+ *                 property="data",
+ *                 type="object",  
+ *                  @OA\Property(
+ *                      property="id",
+ *                      type="integer",
+ *                      example=1
+ *                  ),
+ *                  @OA\Property(
+ *                      property="title",
+ *                      type="string",
+ *                      example="Some body"
+ *                  ),
+ *                  @OA\Property(
+ *                      property="like",
+ *                      type="integer",
+ *                      example=20
+ *                   ),
+ *               ),
+ *          )
+ *     )
+ * ),  
  * 
+ * @OA\Patch(
+ *     path="/api/posts/{post}",
+ *     summary="Обновление",
+ *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
+ *     @OA\Parameter(
+ *         description="ID поста",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=2,
+ *     ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(
+ *                 property="title",
+ *                 type="string",
+ *                 example="Some body"
+ *             ),
+ *              @OA\Property(
+ *                 property="like",
+ *                 type="integer",
+ *                 example=20
+ *              ),
+ *          )
+ *      ),
  * 
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *              @OA\Property(
+ *                 property="data",
+ *                 type="object",  
+ *                  @OA\Property(
+ *                      property="id",
+ *                      type="integer",
+ *                      example=1
+ *                  ),
+ *                  @OA\Property(
+ *                      property="title",
+ *                      type="string",
+ *                      example="Some body"
+ *                  ),
+ *                  @OA\Property(
+ *                      property="like",
+ *                      type="integer",
+ *                      example=20
+ *                   ),
+ *               ),
+ *          )
+ *     )
+ * ),  
+ * 
+ * @OA\Delete(
+ *     path="/api/posts/{post}",
+ *     summary="Удаление",
+ *     tags={"Post"},
+ *     security={{ "bearerAuth": {} }},
+ *     @OA\Parameter(
+ *         description="ID поста",
+ *         in="path",
+ *         name="post",
+ *         required=true,
+ *         example=1,
+ *     ),
+ * 
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *              @OA\Property(
+ *                 property="message",
+ *                 type="string", 
+ *                 example="done" 
+ *               ),
+ *          )
+ *     )
+ * ), 
  */
 class PostController extends Controller
 {
-   
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,28 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::resource('posts', PostController::class);
+Route::resource('posts', PostController::class)->middleware('jwt.auth');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
